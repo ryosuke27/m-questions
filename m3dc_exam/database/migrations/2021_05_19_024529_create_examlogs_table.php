c987c1cf62a1f32644984771e752168004a10450
{ryosuke27} <opu.7455@gmail.com>
2021-05-19 15:58:23 +0900

    char length

--- m3dc_exam/database/migrations/2021_05_19_024529_create_examlogs_table.php
+++ m3dc_exam/database/migrations/2021_05_19_024529_create_examlogs_table.php
@@ -15,14 +15,14 @@ class CreateExamlogsTable extends Migration
     {
         Schema::create('exam_log', function (Blueprint $table) {
             $table->increments('id');
-            $table->date("crnt_date");
-            $table->string("todohuken");
-            $table->string("fname");
-            $table->string("lname");
+            $table->dateTime("crnt_date");
+            $table->string("todohuken", 50);
+            $table->string("fname", 10);
+            $table->string("lname", 15);
             $table->integer("viewcnt");
-            $table->string("ip_addr");
-            $table->string("referer");
-            $table->string("usr_agent");
+            $table->string("ip_addr", 30);
+            $table->string("referer", 200);
+            $table->string("usr_agent", 200);
         });
     }
 
