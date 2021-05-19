841fbdb8bd511f4b2a2b8252fb07d9d521159f77
{ryosuke27} <opu.7455@gmail.com>
2021-05-19 16:02:47 +0900

    table

--- m3dc_exam/app/Examlog.php
+++ m3dc_exam/app/Examlog.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Examlog extends Model
 {
-    //
+    /**
+     * モデルと関連しているテーブル
+     *
+     * @var string
+     */
+    protected $table = 'exam_log';
 }
