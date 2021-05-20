863fd9aa21b653a7f8cb2bdc79eb65854d11c5e2
{ryosuke27} <opu.7455@gmail.com>
2021-05-20 23:16:36 +0900

    store

--- m3dc_exam/app/Http/Controllers/InputController.php
+++ m3dc_exam/app/Http/Controllers/InputController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class InputController extends Controller
 {
@@ -19,6 +20,17 @@ class InputController extends Controller
             'fname' => 'required|max:50',
             'viewcnt' => 'required|integer',
         ]);
+
+        DB::table('exam_log')->insert([
+            'todohuken' => $request->todohuken,
+            'lname' => $request->lname,
+            'fname' => $request->fname,
+            'viewcnt' => $request->viewcnt,
+            'ip_addr' => $request->ip(),
+            'referer' => $request->header('referer'),
+            'usr_agent' => $request->header('User-Agent'),
+        ]);
+
 
         return view('viewpages.viewpage');
     }
