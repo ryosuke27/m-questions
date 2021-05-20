fc50752546297c9932b382c853a827f35d52c90f
{ryosuke27} <opu.7455@gmail.com>
2021-05-20 19:43:50 +0900

    iframe

--- m3dc_exam/resources/views/viewpages/viewpage.blade.php
+++ m3dc_exam/resources/views/viewpages/viewpage.blade.php
@@ -2,31 +2,42 @@
 
 @section('content')
 
-	<div class="container">
-		<div class="container-component">
+<div class="container">
+    <div class="container-component">
 
-        	<div class="pageWrap">
-                <div class="panel panel-primary">
-                    <div class="panel-heading text-center"></div>
-                    <div class="panel-body">
-                        <pre class="list-unstyled "></pre>
+        <div class="pageWrap">
+            <div class="panel panel-primary">
+                <div class="panel-heading text-center"></div>
+                <div class="panel-body">
+                    <pre class="list-unstyled">
+                        <div class="gaiyo">
+							<dl>
+                          		<dt>日程</dt>
+								<dd>{{ config('defaultcfg')['defaultcfg']['SEMI_INFO_DATE'] }}</dd>
+								<dt>演題</dt>
+								<dd>{{ config('defaultcfg')['defaultcfg']['SEMI_INFO_TITLE'] }}</dd>
+								<dt>演者</dt>
+								<dd>{{ config('defaultcfg')['defaultcfg']['SEMI_INFO_PROF'] }}</dd>
+               				</dl>
+						</div>
+                    </pre>
 
-                        <div>
-                        	<!-- iframe コード入力 -->
-                        </div>
-
+                    <div>
+                        <iframe src="/img/m3dc_logo.png"></iframe>
                     </div>
-                    <div class="panel-footer">
-                        <p class="glyphicon glyphicon-warning-sign text-danger">　PCでご視聴の場合はVPNを切断しご覧ください</p>
-                    </div>
+
                 </div>
+                <div class="panel-footer">
+                    <p class="glyphicon glyphicon-warning-sign text-danger">　PCでご視聴の場合はVPNを切断しご覧ください</p>
+                </div>
+            </div>
 
-                <div class="col-sm-12 contactBox">
-                	<a target="_blank" href="">接続に関する技術的な質問等ございましたら、こちらからお問い合わせ下さい。</a>
-            	</div>
-	        </div>
-
+            <div class="col-sm-12 contactBox">
+                <a target="_blank" href="">接続に関する技術的な質問等ございましたら、こちらからお問い合わせ下さい。</a>
+            </div>
         </div>
+
     </div>
+</div>
 
 @endsection
